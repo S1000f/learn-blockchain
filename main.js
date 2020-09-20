624ec731f44b80be2528f111274820acb6251ab8
dohpkim <dohp.kim@gmail.com>
2020-09-20 11:38:50 +0900

    p2p network for blockchain

--- main.js
+++ main.js
@@ -146,6 +146,16 @@ function addBlock(newBlock) {
     return true;
 }
 
+function mineBlock(blockData) {
+    const newBlock = generateNextBlock(blockData);
+    if (addBlock(blockData)) {
+        broadcast(responseLatestMsg());
+        return newBlock;
+    } else {
+        return null;
+    }
+}
+
 // Chapter 2
 const http_port = process.env.HTTP_PORT || 3001;
 
@@ -161,10 +171,12 @@ function initHttpServer() {
     })
     app.post("/mineBlock", function (req, res) {
         const data = req.body.data || [];
-        const newBlock = generateNextBlock(data);
-        addBlock(newBlock);
-
-        res.send(newBlock);
+        const newBlock = mineBlock(data);
+        if (newBlock === null) {
+            res.status(400).send('Bad Request');
+        } else {
+            res.send(newBlock);
+        }
     })
     app.get("/version", function (req, res) {
         res.send(getCurrentVersion());
@@ -173,11 +185,191 @@ function initHttpServer() {
         res.send({ "msg": "stopping server"});
         process.exit();
     })
+    app.get("/peers", function (req, res) {
+        res.send(getSockets().map(function (s) {
+            return s._socket.remoteAddress + ":" + s._socket.remotePort;
+        }))
+    })
+    app.post("/addPeers", function (req, res) {
+        const peers = req.body.peers || [];
+        connectToPeers(peers);
+        res.send();
+    })
 
     app.listen(http_port, function () {
         console.log("Listening http port on : " + http_port)
     })
 }
-
 initHttpServer();
+
+const p2p_port = process.env.P2P_PORT || 6001;
+const WebSocket = require("ws");
+
+function initP2PServer() {
+    const server = new WebSocket.Server({
+        port: p2p_port
+    });
+    server.on("connection", function (ws) {
+        initConnection(ws);
+    })
+    console.log("Listening websocket p2p port on: " + p2p_port);
+}
+initP2PServer();
+
+function connectToPeers(newPeers) {
+    newPeers.forEach(function (peer) {
+        const ws = new WebSocket(peer);
+        ws.on("open", function () {
+            initConnection(ws);
+        });
+        ws.on("error", function () {
+            console.log("Connection faild...");
+        })
+    })
+}
+
+var sockets = [];
+
+function getSockets() {
+    return sockets;
+}
+
+function initConnection(ws) {
+    sockets.push(ws);
+    initMessageHandler(ws);
+    initErrorHandler(ws);
+    write(ws, queryChainLengthMsg());
+}
+
+function write(ws, message) {
+    ws.send(JSON.stringify(message));
+}
+
+function broadcast(message) {
+    sockets.forEach(function (socket) {
+        write(socket, message);
+    })
+}
+
+const MessageType = {
+    QUERY_LATEST: 0,
+    QUERY_ALL: 1,
+    RESPONSE_BLOCKCHAIN: 2
+};
+
+function initMessageHandler(ws) {
+    ws.on("message", function (data) {
+        const message = JSON.parse(data);
+
+        switch (message.type) {
+            case MessageType.QUERY_LATEST:
+                write(ws, responseLatestMsg());
+                break;
+            case MessageType.QUERY_ALL:
+                write(ws, responseChainMsg());
+                break;
+            case MessageType.RESPONSE_BLOCKCHAIN:
+                handleBlockchainResponse(message);
+                break;
+        }
+    })
+}
+
+function queryAllMsg() {
+    return ({
+        "type": MessageType.QUERY_ALL,
+        "data": null
+    });
+}
+
+function queryChainLengthMsg() {
+    return ({
+        "type": MessageType.QUERY_LATEST,
+        "data": null
+    });
+}
+
+function responseChainMsg() {
+    return ({
+        "type": MessageType.RESPONSE_BLOCKCHAIN,
+        "data": JSON.stringify(getBlockchain())
+    });
+}
+
+function responseLatestMsg() {
+    return ({
+        "type": MessageType.RESPONSE_BLOCKCHAIN,
+        "data": JSON.stringify([getLatestBlock()])
+    });
+}
+
+function handleBlockchainResponse(message) {
+    const receiveBlocks = JSON.parse(message.data);
+    const latestBlockReceived = receiveBlocks[receiveBlocks.length - 1];
+    const latestBlockHeld = getLatestBlock();
+
+    if (latestBlockReceived.header.index > latestBlockHeld.header.index) {
+        console.log(
+            "Blockchain possibly behind." +
+            "We got: " + latestBlockHeld.header.index + ", " +
+            "Peer got: " + latestBlockReceived.header.index
+        );
+        if (calculateHashForBlock(latestBlockHeld) === latestBlockReceived.header.previousHash) {
+            // in this case, the received block refers the latest block of my ledger
+            console.log("We can append the received block to our chain...");
+            if (addBlock(latestBlockReceived)) {
+                broadcast(responseLatestMsg());
+            }
+        } else if (receiveBlocks.length === 1) {
+            // in this case, need to reorganize chain
+            console.log("We have to query the chain from our peer");
+            broadcast(queryAllMsg());
+        } else {
+            // replace chain
+            console.log("The received block is longer than current blockchain...");
+            replaceChain(receiveBlocks);
+        }
+    } else {
+        console.log("the received block is not longer than current blockchain. Do nothing...");
+    }
+}
+
+function initErrorHandler(ws) {
+    ws.on("close", function () {
+        closeConnection(ws);
+    })
+    ws.on("error", function () {
+        closeConnection(ws);
+    })
+}
+
+function closeConnection(ws) {
+    console.log("connection failed to peer: " + ws.url);
+    sockets.splice(sockets.indexOf(ws), 1);
+}
+
+const random = require("random");
+
+function replaceChain(newBlock) {
+    if (
+        isValidChain(newBlock) &&
+        (newBlock.length > blockchain.length || (newBlock.length === blockchain.length) && random.boolean())
+    ) {
+        console.log("the received blockchain is valid. Replacing current blockchain with the received one...");
+        blockchain = newBlocks;
+        broadcast(responseLatestMsg());
+    } else {
+        console.log("invalid blockchain received...")
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
